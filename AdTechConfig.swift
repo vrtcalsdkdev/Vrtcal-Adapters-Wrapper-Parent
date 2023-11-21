0b59c043ca32f8d6ae9f422e4e05655df09e412c
Scott McCoy <scott.mccoy@vrtcal.com>
2023-11-21 14:12:53 -0800

    fix: AdTechConfig.name is now generated from components
    Fixed vrtcalToIronSourceInterstitial pointing at fyberMarketplace instead

--- AdTechConfig.swift
+++ AdTechConfig.swift
@@ -1,9 +1,16 @@
 import Foundation
 
 public struct AdTechConfig {
-    public let name: String
     public let primarySdk: SDK
-    public let secondarySDK: SDK
+    public let secondarySdk: SDK
     public let placementType: PlacementType
     public let adUnitId: String
+    
+    public var name: String {
+        if secondarySdk == .none {
+            return "\(primarySdk) Solo \(placementType)"
+        } else {
+            return "\(primarySdk) to \(secondarySdk) \(placementType)"
+        }
+    }
 }
--- AdTechConfigProvider.swift
+++ AdTechConfigProvider.swift
@@ -47,135 +47,120 @@ public enum AdTechConfigProvider: CaseIterable, CustomStringConvertible {
         // MARK: Vrtcal
         case .vrtcalSoloBanner:
             return AdTechConfig(
-                name: "Vrtcal Solo Banner",
                 primarySdk: .vrtcal,
-                secondarySDK: .none,
+                secondarySdk: .none,
                 placementType: .banner,
                 adUnitId: "10005"
             )
         
         case .vrtcalSoloInterstitial:
             return AdTechConfig(
-                name: "Vrtcal Solo Interstitial",
                 primarySdk: .vrtcal,
-                secondarySDK: .none,
+                secondarySdk: .none,
                 placementType: .interstitial,
                 adUnitId: "10009"
             )
 
         case .vrtcalToAdMobBanner:
             return AdTechConfig(
-                name: "Vrtcal to AdMob Banner",
                 primarySdk: .vrtcal,
-                secondarySDK: .adMob,
+                secondarySdk: .adMob,
                 placementType: .banner,
                 adUnitId: "10231"
             )
 
         case .vrtcalToAdMobInterstitial:
             return AdTechConfig(
-                name: "Vrtcal to AdMob Interstitial",
                 primarySdk: .vrtcal,
-                secondarySDK: .adMob,
+                secondarySdk: .adMob,
                 placementType: .interstitial,
                 adUnitId: "10232"
             )
 
         case .vrtcalToAppLovinBanner:
             return AdTechConfig(
-                name: "Vrtcal to AppLovin Banner",
                 primarySdk: .vrtcal,
-                secondarySDK: .appLovin,
+                secondarySdk: .appLovin,
                 placementType: .banner,
                 adUnitId: "10399"
             )
         
         case .vrtcalToAppLovinInterstitial:
             return AdTechConfig(
-                name: "Vrtcal to AppLovin Interstitial",
                 primarySdk: .vrtcal,
-                secondarySDK: .appLovin,
+                secondarySdk: .appLovin,
                 placementType: .interstitial,
                 adUnitId: "10407"
             )
         
         case .vrtcalToFyberFairBidInterstitial:
             return AdTechConfig(
-                name: "Vrtcal to AppLovin Interstitial",
                 primarySdk: .vrtcal,
-                secondarySDK: .fyberFairbid,
+                secondarySdk: .fyberFairbid,
                 placementType: .interstitial,
                 adUnitId: "10418"
             )
             
         case .vrtcalToFyberMarketplaceBanner:
             return AdTechConfig(
-                name: "Vrtcal to Fyber Marketplace Banner",
                 primarySdk: .vrtcal,
-                secondarySDK: .fyberMarketplace,
+                secondarySdk: .fyberMarketplace,
                 placementType: .banner,
                 adUnitId: "10419"
             )
             
         case .vrtcalToFyberMarketplaceInterstitial:
             return AdTechConfig(
-                name: "Vrtcal to Fyber Marketplace Interstitial",
                 primarySdk: .vrtcal,
-                secondarySDK: .fyberMarketplace,
+                secondarySdk: .fyberMarketplace,
                 placementType: .interstitial,
                 adUnitId: "10420"
             )
             
         case .vrtcalToIronSourceInterstitial:
             return AdTechConfig(
-                name: "Vrtcal to Fyber Marketplace Interstitial",
                 primarySdk: .vrtcal,
-                secondarySDK: .fyberMarketplace,
+                secondarySdk: .ironSource,
                 placementType: .interstitial,
                 adUnitId: "10417"
             )
             
         case .vrtcalToSmaatoBanner:
             return AdTechConfig(
-                name: "Vrtcal to Smaato Banner",
                 primarySdk: .vrtcal,
-                secondarySDK: .smaato,
+                secondarySdk: .smaato,
                 placementType: .banner,
                 adUnitId: "10498"
             )
             
         case .vrtcalToSmaatoInterstitial:
             return AdTechConfig(
-                name: "Vrtcal to Smaato Interstitial",
                 primarySdk: .vrtcal,
-                secondarySDK: .smaato,
+                secondarySdk: .smaato,
                 placementType: .interstitial,
                 adUnitId: "10499"
             )
             
         case .vrtcalToTapjoyInterstitial:
             return AdTechConfig(
-                name: "Vrtcal to Tapjoy Interstitial",
                 primarySdk: .vrtcal,
-                secondarySDK: .tapjoy,
+                secondarySdk: .tapjoy,
                 placementType: .interstitial,
                 adUnitId: "10502"
             )
             
         case .vrtcalToVungleBanner:
             return AdTechConfig(
-                name: "Vrtcal to Vungle Banner",
                 primarySdk: .vrtcal,
-                secondarySDK: .vungle,
+                secondarySdk: .vungle,
                 placementType: .banner,
                 adUnitId: "10478"
             )
             
         case .vrtcalToVungleInterstitial:
             return AdTechConfig(
-                name: "Vrtcal to Vungle Interstitial",
                 primarySdk: .vrtcal,
-                secondarySDK: .vungle,
+                secondarySdk: .vungle,
                 placementType: .interstitial,
                 adUnitId: "10501"
             )
@@ -183,18 +168,16 @@ public enum AdTechConfigProvider: CaseIterable, CustomStringConvertible {
         // MARK: AdMob
         case .adMobToVrtcalBanner:
             return AdTechConfig(
-                name: "AdMob to Vrtcal Banner",
                 primarySdk: .adMob,
-                secondarySDK: .vrtcal,
+                secondarySdk: .vrtcal,
                 placementType: .banner,
                 adUnitId: "ca-app-pub-9576567786990145/7712119624"
             )
             
         case .adMobToVrtcalInterstitial:
             return AdTechConfig(
-                name: "AdMob to Vrtcal Interstitial",
                 primarySdk: .adMob,
-                secondarySDK: .vrtcal,
+                secondarySdk: .vrtcal,
                 placementType: .interstitial,
                 adUnitId: "ca-app-pub-9576567786990145/2025798230"
             )
@@ -202,27 +185,24 @@ public enum AdTechConfigProvider: CaseIterable, CustomStringConvertible {
         // MARK: AppLovin
         case .appLovinToVrtcalBanner:
             return AdTechConfig(
-                name: "AppLovin to Vrtcal Banner",
                 primarySdk: .appLovin,
-                secondarySDK: .vrtcal,
+                secondarySdk: .vrtcal,
                 placementType: .banner,
                 adUnitId: "0bbf2c625c63e29f"
             )
             
         case .appLovinToVrtcalInterstitial:
             return AdTechConfig(
-                name: "AppLovin to Vrtcal Interstitial",
                 primarySdk: .appLovin,
-                secondarySDK: .vrtcal,
+                secondarySdk: .vrtcal,
                 placementType: .interstitial,
                 adUnitId: "47f3a9b0433d25a4"
             )
             
         case .appLovinDebugView:
             return AdTechConfig(
-                name: "AppLovin Debug View",
                 primarySdk: .appLovin,
-                secondarySDK: .none,
+                secondarySdk: .none,
                 placementType: .showDebugView,
                 adUnitId: ""
             )
@@ -232,18 +212,16 @@ public enum AdTechConfigProvider: CaseIterable, CustomStringConvertible {
         // MARK: IronSource
         case .ironSourceToVrtcalInterstitial:
             return AdTechConfig(
-                name: "IronSource to Vrtcal Interstitial",
                 primarySdk: .ironSource,
-                secondarySDK: .vrtcal,
+                secondarySdk: .vrtcal,
                 placementType: .interstitial,
                 adUnitId: "556d9907930e44b492158fef75f8724b"
             )
 
         case .ironSourceToVrtcalRewardedVideo:
             return AdTechConfig(
-                name: "IronSource to Vrtcal Rewarded Video",
                 primarySdk: .ironSource,
-                secondarySDK: .vrtcal,
+                secondarySdk: .vrtcal,
                 placementType: .rewardedVideo,
                 adUnitId: ""
             )
@@ -253,18 +231,16 @@ public enum AdTechConfigProvider: CaseIterable, CustomStringConvertible {
         // Ad unit IDs from https://developers.smaato.com/publishers/nextgen-sdk-ios-integration/
         case .smaatoSoloBanner:
             return AdTechConfig(
-                name: "Smaato Solo Banner",
                 primarySdk: .smaato,
-                secondarySDK: .none,
+                secondarySdk: .none,
                 placementType: .banner,
                 adUnitId: "130626424"
             )
 
         case .smaatoSoloInterstitial:
             return AdTechConfig(
-                name: "Smaato Solo Interstitial",
                 primarySdk: .smaato,
-                secondarySDK: .none,
+                secondarySdk: .none,
                 placementType: .interstitial,
                 adUnitId: "130626426"
             )
@@ -272,13 +248,11 @@ public enum AdTechConfigProvider: CaseIterable, CustomStringConvertible {
         // MARK: Tapjoy
         case .tapjoySoloInterstitial:
             return AdTechConfig(
-                name: "Tapjoy Solo Interstitial",
                 primarySdk: .tapjoy,
-                secondarySDK: .none,
+                secondarySdk: .none,
                 placementType: .interstitial,
                 adUnitId: "QA CORE Interstitial iOS Local"
             )
-
         }
     }
 }
